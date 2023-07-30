75328248ab967f5d8e73619768de0214fb8294fc
jakir71 <114278240+jakir71@users.noreply.github.com>
2023-07-30 18:31:55 +0600

    Add files via upload

---
+++ AI Code/Prolog/fileHandling.pl
@@ -0,0 +1,19 @@
+writeinfile:-
+ tell('mahfuz.txt'),
+ write(' \'I\' . '),
+ write(' \'Am\' . '),
+ write('\'Mahfuz\' . '),
+ told.
+readfromfile(Infile,Outfile):-
+ see(Infile),
+ tell(Outfile),
+ read(X1),
+ write(X1),
+ nl,
+ read(X2),
+ write(X2),
+ nl,
+ read(X3),
+ write(X3),
+ seen,
+ told.
