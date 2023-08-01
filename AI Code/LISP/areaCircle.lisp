07784639a4c946e19d4b444c0c79b447496df58f
jakir71 <114278240+jakir71@users.noreply.github.com>
2023-08-01 16:17:34 +0600

    Delete AI Code directory

--- AI Code/LISP/areaCircle.lisp
+++
@@ -1,5 +0,0 @@
-(format t "Enter the radius: " )
-(setq r (read))
-
-(setq area (* pi r r))
-(format t"Circle Area: ~,2f~%" area)
--- AI Code/LISP/array.lisp
+++
@@ -1,5 +0,0 @@
-(setq arr #(10 20 30 40 50))
-
-(format t "Array: ")
-(dotimes (i (length arr))
-    (format t "~a " (aref arr i)))
